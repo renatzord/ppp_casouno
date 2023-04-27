72e48e70edc3ef4677abceb6dc15e5f550c7f5ee
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-04-27 18:26:48 -0500

    Entidades subida 1

---
+++ ppp/src/main/java/com/api/ppp/back/models/Visita_Actividad.java
@@ -0,0 +1,28 @@
+package com.api.ppp.back.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "visita_actividad")
+public class Visita_Actividad implements Serializable {
+
+    @Id
+    @Column(name = "vac_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "vac_actividad")
+    private String actividad;
+
+    @Column(name = "vac_observacion")
+    private String observacion;
+
+    @ManyToOne
+    @JoinColumn(name = "vit_id",referencedColumnName = "vit_id")
+    private Visita visita;
+
+}
