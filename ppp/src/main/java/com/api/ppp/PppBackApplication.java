d67a7c8dba75cb62c9079d8728124d09a5724691
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-04-26 20:14:05 -0500

    Creacion de Repositorio

---
+++ ppp/src/main/java/com/api/ppp/PppBackApplication.java
@@ -0,0 +1,13 @@
+package com.api.ppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppBackApplication.class, args);
+	}
+
+}
---
+++ ppp/src/test/java/com/api/ppp/PppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
