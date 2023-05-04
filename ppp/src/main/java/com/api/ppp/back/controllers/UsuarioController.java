7a192c8c91788f4bacdd2e58ac0de8795fe4f7c0
Andres Zuin <jonathan.zuin.est@tecazuay.edu.ec>
2023-05-03 21:05:32 -0500

    User Controller

--- ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
+++ ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
@@ -1,9 +1,13 @@
 package com.api.ppp.back.controllers;
 
+import com.api.ppp.back.models.Usuario;
 import com.api.ppp.back.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -11,5 +15,27 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
+
+    // To list all records
+    @GetMapping("/listar")
+    public ResponseEntity<?> listar() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    // To find one record, specifically by a unique identifier (PK or ID)
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<?> buscarID(@PathVariable("id") Integer id) {
+        Optional<Usuario> current = service.findById(id);
+        if(current.isPresent()) {
+            return ResponseEntity.ok().body(current.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // To create a record
+    @PostMapping("/crear")
+    public ResponseEntity<?> crear(@RequestBody Usuario entity) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(entity));
+    }
 
 }
