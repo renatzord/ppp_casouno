b1297c4e3c691823666bbbf1676bd18b26701108
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-05-08 19:24:35 -0500

    Prueba renato

---
+++ ppp/src/main/java/com/api/ppp/fenix/fcontrollers/CarrerasFenix.java
@@ -0,0 +1,25 @@
+package com.api.ppp.fenix.fcontrollers;
+
+import com.api.ppp.fenix.fmodels.UsuarioFenix;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/fenix")
+public class CarrerasFenix {
+/*
+    @GetMapping("/llenarcarreras")
+    public ResponseEntity<?> buscarCedula() {
+
+        if(current.isPresent()) {
+            return ResponseEntity.ok().body(current.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+*/
+}
