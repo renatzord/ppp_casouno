f23f97a347fbffdceb96bcabb7091afb8beb42db
Andres Zuin <jonathan.zuin.est@tecazuay.edu.ec>
2023-05-23 16:29:22 -0500

    Create usuario activated

--- ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
+++ ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
@@ -1,14 +1,19 @@
 package com.api.ppp.back.controllers;
 
 import com.api.ppp.back.daos.AuthorityRepository;
+import com.api.ppp.back.models.Authority;
+import com.api.ppp.back.models.TutorInstituto;
 import com.api.ppp.back.models.Usuario;
 import com.api.ppp.back.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
+import static com.api.ppp.back.constant.Validate.isPasswordSecure;
 
 @RestController
 @RequestMapping("/usuario")
@@ -20,6 +25,9 @@ public class UsuarioController {
 
     @Autowired
     private AuthorityRepository authorityRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     // To list all records
     @GetMapping("/listar")
@@ -39,8 +47,18 @@ public class UsuarioController {
 
     // To create a record
     @PostMapping("/crear")
-    public ResponseEntity<?> crear(@RequestBody Usuario entity) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(entity));
+    public ResponseEntity<?> crear(@RequestBody Usuario entity, @RequestParam String rol) {
+        if (!isPasswordSecure(entity.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST) .body("Password must be secure");
+        }
+        String hashPwd = passwordEncoder.encode(entity.getPassword());
+        entity.setPassword(hashPwd);
+        Usuario usuario = service.save(entity);
+        Authority role = new Authority();
+        role.setName(rol);
+        role.setUsuario(usuario);
+        authorityRepository.save(role);
+        return ResponseEntity .status(HttpStatus.CREATED).body("Usuario registrado con exito.");
     }
 
     // To list users by their roles
