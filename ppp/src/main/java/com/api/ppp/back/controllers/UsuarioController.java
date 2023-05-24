e7b789de0cac58c05d95620f6bd244e2865ed793
Andres Zuin <jonathan.zuin.est@tecazuay.edu.ec>
2023-05-24 17:07:46 -0500

    D;

--- ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
+++ ppp/src/main/java/com/api/ppp/back/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.api.ppp.back.controllers;
 
 import com.api.ppp.back.daos.AuthorityRepository;
+import com.api.ppp.back.models.Authority;
 import com.api.ppp.back.models.Usuario;
 import com.api.ppp.back.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -47,6 +49,25 @@ public class UsuarioController {
     @GetMapping("/listar/rol")
     public ResponseEntity<?> listarRol(String rol) {
         return ResponseEntity.ok().body(authorityRepository.findByName(rol));
+    }
+
+    // to list all records in table authorities
+    @GetMapping("/authority/listar")
+    public ResponseEntity<?> buscarAuth() {
+        List<Authority> authorities = authorityRepository.findAll();
+        return ResponseEntity.ok().body(authorities);
+    }
+
+    // To update a user's role name just by its name
+    @PostMapping("/authority/editar/{id}")
+    public ResponseEntity<?> editarAuthority(@PathVariable("id") Integer id, @RequestParam String name) {
+        Optional<Authority> current = authorityRepository.findById(id);
+        if (current.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Authority authority = current.get();
+        authority.setName(name);
+        return ResponseEntity.ok().body(authorityRepository.save(authority));
     }
 
 }
