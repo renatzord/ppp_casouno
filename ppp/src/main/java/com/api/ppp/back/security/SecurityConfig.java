2bdf01ed23911ffcc223e8d265ac672d8bc2bfe5
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-05-31 00:37:43 -0500

    config prueba crsitian v2

--- ppp/src/main/java/com/api/ppp/back/security/SecurityConfig.java
+++ ppp/src/main/java/com/api/ppp/back/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:4200/")); // Reemplaza "http://example.com" con tu dominio permitido o patrón
+                        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:58697"));// Reemplaza "http://example.com" con tu dominio permitido o patrón
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
